2d45c44d5cad93d99bd1f4899485ceea4169b960
Emily Böhm <5203458+EmilyMalkieri@users.noreply.github.com>
2025-01-26 03:36:50 +0100

    Wow apparently the CI is very particular about how this file should look

--- Casks/scrivener-compile-to-ao3.rb
+++ Casks/scrivener-compile-to-ao3.rb
@@ -1,11 +1,11 @@
 cask "scrivener-compile-to-ao3" do
-  desc "Scrivener compile format for the AO3 HTML editor"
-  homepage "https://github.com/EmilyMalkieri/scrivener-compile-to-ao3"
-
   version "1.0.0"
   sha256 "1340432f2afdc65bce06b19e5db00d8d5865bb1744c0b68865dee8c4513e95b7"
 
   url "https://github.com/EmilyMalkieri/scrivener-compile-to-ao3/archive/refs/tags/v#{version}.tar.gz"
+  name "Scrivener Compile Format for AO3"
+  desc "Scrivener compile format for the AO3 HTML editor"
+  homepage "https://github.com/EmilyMalkieri/scrivener-compile-to-ao3"
 
   artifact "scrivener-compile-to-ao3-#{version}/AO3 Fanfic (for HTML editor).scrformat", target: "~/Library/Application Support/Scrivener/Compile Formats/AO3 Fanfic (for HTML editor).scrformat"
 
