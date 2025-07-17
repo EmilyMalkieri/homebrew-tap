253fad95a0c00a57d305b9c79e37e25dfe2fefda
Emily Böhm <5203458+EmilyMalkieri@users.noreply.github.com>
2025-07-17 18:51:50 +0200

    Update compile format to 1.2.0

--- Casks/scrivener-compile-to-ao3.rb
+++ Casks/scrivener-compile-to-ao3.rb
@@ -1,6 +1,6 @@
 cask "scrivener-compile-to-ao3" do
-  version "1.1.0"
-  sha256 "3a3e22d0431119eb504f1e139ff3d0c26d902899031a0078c02d698566e49394"
+  version "1.2.0"
+  sha256 "d720bf640c2a900e354a3f7b7a1d7108ea1facfbff23b2a71ac79d40eb0c0591"
 
   url "https://github.com/EmilyMalkieri/scrivener-compile-to-ao3/archive/refs/tags/v#{version}.tar.gz"
   name "Scrivener Compile Format for AO3"
@@ -8,6 +8,8 @@ cask "scrivener-compile-to-ao3" do
   homepage "https://github.com/EmilyMalkieri/scrivener-compile-to-ao3"
 
   artifact "scrivener-compile-to-ao3-#{version}/AO3 Fanfic (for HTML editor).scrformat", target: "~/Library/Application Support/Scrivener/Compile Formats/AO3 Fanfic (for HTML editor).scrformat"
+  artifact "scrivener-compile-to-ao3-#{version}/Fanfic Manuscript (Times).scrformat", target: "~/Library/Application Support/Scrivener/Compile Formats/Fanfic Manuscript (Times).scrformat"
 
   uninstall trash: "~/Library/Application Support/Scrivener/Compile Formats/AO3 Fanfic (for HTML editor).scrformat"
+  uninstall trash: "~/Library/Application Support/Scrivener/Compile Formats/Fanfic Manuscript (Times).scrformat"
 end
