d8ff60ac70ebb90f3ec6107d72400c0893ba51b8
Emily Böhm <5203458+EmilyMalkieri@users.noreply.github.com>
2025-09-22 19:30:36 +0200

    Upgrade scrivener-compile-to-ao3

--- Casks/scrivener-compile-to-ao3.rb
+++ Casks/scrivener-compile-to-ao3.rb
@@ -1,6 +1,6 @@
 cask "scrivener-compile-to-ao3" do
-  version "1.2.0"
-  sha256 "d720bf640c2a900e354a3f7b7a1d7108ea1facfbff23b2a71ac79d40eb0c0591"
+  version "1.2.1"
+  sha256 "e242c4acd3fccf05f5ceeb53d2b5b575a9b45ce261eb06f8a0ce73c83b9add46"
 
   url "https://github.com/EmilyMalkieri/scrivener-compile-to-ao3/archive/refs/tags/v#{version}.tar.gz"
   name "Scrivener Compile Format for AO3"
